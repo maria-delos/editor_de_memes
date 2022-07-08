f3d96cd81c00431bc5bfeb959baad7d9fc70ef42
maria de los angeles <mariadeloshernandez15@hotmail.com>
2022-07-08 15:43:20 -0400

    casi terminando el modo oscuro, solo faltan pequenios detalles

--- javascript/editor_memes.js
+++ javascript/editor_memes.js
@@ -5,7 +5,10 @@ const asideTexto = document.querySelector('.div-texto')
 const btnNavImagen = document.querySelector('#btn-imagen')
 const btnNavTexto = document.querySelector('#btn-texto')
 const btnModoOscuro = document.querySelector('#btn-modo-oscuro')
-
+const fondoTodo = document.querySelector('#fondo-home')
+const encabezado = document.querySelector('.encabezado')
+const fondoAside = document.querySelector('aside')
+const btnNavTodos = document.querySelectorAll('.btn-nav')
 
 
 btnNavImagen.addEventListener('click', ()=>{
@@ -15,10 +18,12 @@ btnNavImagen.addEventListener('click', ()=>{
 })
 
 btnNavTexto.addEventListener('click', ()=>{
-    
     asideImagen.style.display = "none"
     asideTexto.style.display = "block"
-    
-    
-
 })
+
+// btnModoOscuro.addEventListener('click', ()=>{  //traerme las variables locales
+//     encabezado.classList.toggle('navegador')
+//     fondoTodo.classList.toggle('fondoMain')
+//     fondoAside.classList.toggle('fondoAside')
+//})
