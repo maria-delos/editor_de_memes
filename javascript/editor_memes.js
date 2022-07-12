dfc1a0ace947d1c855b93ebd87e1a37ea18a10aa
maria de los angeles <mariadeloshernandez15@hotmail.com>
2022-07-12 03:45:38 -0400

    agregue los filtros pero aun no andan

--- javascript/editor_memes.js
+++ javascript/editor_memes.js
@@ -10,6 +10,7 @@ const encabezado = document.querySelector('.encabezado')
 const fondoAside = document.querySelector('aside')
 const btnNavTodos = document.querySelectorAll('.btn-nav')
 const body = document.querySelector('body')
+const img = document.querySelector('#cont-imagen') // caja meme
 
 btnNavImagen.addEventListener('click', ()=>{
     asideImagen.style.display = "block"
@@ -22,23 +23,88 @@ btnNavTexto.addEventListener('click', ()=>{
 })
 
 ////////////////////   MODO OSCURO   /////////////////////
+
 btnModoOscuro.addEventListener('click', ()=>{
     body.classList.toggle('dark')
 })
 
 
 ////////////////////   IMAGEN MEME  /////////////////////
+
 let inputUrl = document.querySelector('#url')
-let cajaMeme = document.querySelector('#cont-imagen')
+//let cajaMeme = document.querySelector('#cont-imagen')
 
 
 inputUrl.addEventListener("input", (event)=>{
-    let imagenIngresada = event.target.value
-    cajaMeme.innerHTML = `<img src="${imagenIngresada}">`
-    cajaMeme.style.backgroundSize = 'cover'
+   let imagenIngresada = event.target.value
+   // img.innerHTML = `<img src="${imagenIngresada}">`
+   img.style = `background-image: url(${imagenIngresada});`
 })
 
+/////////////////// COLOR DE FONDO DE LA IMG Y MODO  /////////////////////
+
+const colorFondoMeme = document.querySelector('#color-fondo')
+const codigoColor = document.querySelector('#codigo-color')
+
+const modoFusion = document.querySelector('#opcion-color')
+
+colorFondoMeme.addEventListener('input', (event)=>{
+    let valorColor = event.target.value
+    console.log(valorColor)
+    img.style.backgroundColor = valorColor
+    codigoColor.innerHTML = `${valorColor}`
+})
+
+// modo de fusion
+
+modoFusion.addEventListener('input', (event)=>{
+    let valorModoFusion = event.target.value
+    img.style.backgroundBlendMode = valorModoFusion
+})
+
+
+
+
+/////////////////// F I L T R O S  /////////////////////
+
+const iBrillo = document.querySelector('#brillo')
+const iOpacidad = document.querySelector('#opacidad')
+const iContraste = document.querySelector('#contraste')
+const iDesenfoque = document.querySelector('#desenfoque')
+const iEscalaGrises = document.querySelector('#grises')
+const iSepia = document.querySelector('#sepia')
+const iHue = document.querySelector('#hue')
+const iSaturado = document.querySelector('#saturado')
+const iNegativo = document.querySelector('#negativo')
+
+
+
+const filtros = () =>{
+    img.style.filter = `brightness(${iBrillo.value})
+    opacity(${iOpacidad.value})
+    contrast(${iContraste.value}%)
+    blur(${iDesenfoque.value}px)
+    grayscale(${iEscalaGrises.value}%)
+    sepia(${iSepia.value}%)
+    hue-rotate(${iHue.value}deg)
+    saturation(${iSaturado.value}%)
+    invert(${iNegativo.value})`
+
+    console.log(iBrillo.value)
+}
+
+iBrillo.addEventListener('input', filtros)
+iOpacidad .addEventListener('input', filtros)
+iContraste.addEventListener('input', filtros)
+iDesenfoque.addEventListener('input', filtros)
+iEscalaGrises.addEventListener('input', filtros)
+iSepia.addEventListener('input', filtros)
+iHue.addEventListener('input', filtros)
+iSaturado.addEventListener('input', filtros)
+iNegativo.addEventListener('input', filtros)
+
 /////////////////// TOP TEXT  &  BOTTOM TEXT  /////////////////////
+
 let topTextBox = document.querySelector('#top-texto')
 let topTextIngresado = document.querySelector('#text-superior')
 
